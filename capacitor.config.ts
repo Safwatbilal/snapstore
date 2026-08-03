0ab33a67bca324b9dc14963b3d270c627d75b417
safwatbilal <safwetbilal65@gmail.com>
2026-08-03 12:43:24 +0300

    chore: add Capacitor dependencies and configuration file
    - Added @capacitor/cli and @capacitor/core dependencies to package.json
    - Introduced next-pwa dependency for PWA support
    - Created capacitor.config.ts for Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.snapstore.app",
+  appName: "snapstore",
+  webDir: "out",
+};
+
+export default config;
